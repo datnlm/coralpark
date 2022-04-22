355dc3068ac157944c648536786c439f56e847b5
Dat <datnlm@Dats-MacBook-Pro.local>
2022-04-22 16:02:50 +0700

    authorize

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,19 +1,24 @@
 import axios from 'axios';
 
-// axios.defaults.baseURL = process.env.REACT_APP_HOST_API_KEY;
-// axios.defaults.baseURL = 'http://104.45.197.106:8080';
 axios.defaults.baseURL = process.env.REACT_APP_HOST_API_KEY;
-axios.defaults.headers.common = {
-  Authorization: `Bearer ${localStorage.getItem('accessToken')}`
-};
-
 // axios.defaults.headers.common = {
-//   Authorization: `Bearer ${process.env.AUTH_TOKEN}`
+//   Authorization: `Bearer ${localStorage.getItem('accessToken')}`
 // };
-// axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
-// ----------------------------------------------------------------------
 
 const axiosInstance = axios.create();
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    console.log('set accessToken sau khi login');
+    axios.defaults.headers.common = {
+      Authorization: `Bearer ${localStorage.getItem('accessToken')}`
+    };
+    return config;
+  },
+  (error) => {
+    Promise.reject(error);
+  }
+);
 
 axiosInstance.interceptors.response.use(
   (response) => response,
@@ -21,13 +26,3 @@ axiosInstance.interceptors.response.use(
 );
 
 export default axiosInstance;
-
-// axios.defaults.headers.common = {'Authorization': `Bearer ${token}`}
-// export default function setAuthToken(token) {
-//   axios.defaults.headers.common['Authorization'] = '';
-//   delete axios.defaults.headers.common['Authorization'];
-
-//   if (token) {
-//     axios.defaults.headers.common['Authorization'] = `${token}`;
-//   }
-// }
